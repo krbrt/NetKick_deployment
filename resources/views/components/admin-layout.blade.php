f628d00a0f2886be06e72cf77881ca6dfaa577f4
krbrt <kurtragasa0257@gmail.com>
2026-04-11 13:41:41 -0700

    ugh

--- resources/views/components/admin-layout.blade.php
+++ resources/views/components/admin-layout.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>NETKICKS | ADMIN PORTAL</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="antialiased text-white font-sans">
     <div class="flex h-screen overflow-hidden">
