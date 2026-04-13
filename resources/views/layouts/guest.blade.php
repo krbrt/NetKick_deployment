a5e00b8a5e555e50924ee6b761791b407b53b9d9
krtbry <kurtragasa0257@gmail.com>
2026-04-13 11:08:09 +0800

    www

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -11,6 +11,8 @@
 
     <!-- ✅ This is what loads Tailwind CSS -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <script src="https://cdn.tailwindcss.com"></script>
+
 </head>
 <body class="bg-gray-50 text-gray-900 antialiased min-h-screen flex items-center justify-center">
     {{ $slot }}
